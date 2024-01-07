4db62e2df7587dcdf495451232fcfd7eead82b44
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2024-01-07 12:50:56 -0700

    added navbar element and styles

---
+++ src/scripts/components/navbar.js
@@ -0,0 +1,37 @@
+import "styles/navbar.css";
+
+export default class NavBar {
+  constructor(elementToAppendTo, navTitle, links = []) {
+    this.navBar = document.createElement("nav");
+
+    this.navTitleContainer = document.createElement("div");
+    this.navTitleContainer.textContent = navTitle;
+    this.navTitleContainer.classList.add("navTitle");
+    this.navBar.appendChild(this.navTitleContainer);
+
+    this.navBarUnorderedList = document.createElement("ul");
+    this.navBarUnorderedList.classList.add("navHide");
+    this.navBarUnorderedList.classList.add("navList");
+    links.forEach((link) => {
+      const navListItem = document.createElement("li");
+      const navLink = document.createElement("a");
+      navLink.href = link.src;
+      navLink.textContent = link.textContent;
+      navListItem.appendChild(navLink);
+      this.navBarUnorderedList.appendChild(navListItem);
+    });
+    this.navBar.appendChild(this.navBarUnorderedList);
+
+    elementToAppendTo.appendChild(this.navBar);
+
+    this.navBar.addEventListener("mouseover", () => {
+      this.navBarUnorderedList.classList.replace("navHide", "navShow");
+      this.navTitleContainer.classList.add("navActive");
+    });
+
+    this.navBar.addEventListener("mouseleave", () => {
+      this.navBarUnorderedList.classList.replace("navShow", "navHide");
+      this.navTitleContainer.classList.remove("navActive");
+    });
+  }
+}
